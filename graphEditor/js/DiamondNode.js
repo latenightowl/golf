dc0be8650cb403216e6f583e6728a1bd389eb6e4
Thomas Zakh <tommyzakh@gmail.com>
2019-04-21 23:41:40 -0700

    Fixed tabs in DiamondNode.js, LineEdge.js, and CircleNode.js

--- graphEditor/js/DiamondNode.js
+++ graphEditor/js/DiamondNode.js
@@ -1,30 +1,30 @@
 function createDiamondNode (x, y) {
-  return {
-    getBounds: () => {
-      return {
-        x: x,
-        y: y,
-        width: 20,
-        height: 20        
+   return {
+      getBounds: () => {
+         return {
+            x: x,
+            y: y,
+            width: 20,
+            height: 20            
+         }
+      },
+      contains: p => {
+         return (x+20/2-p.x)**2+(y+20/2-p.y)**2 <= 20**2/4
+      },
+      translate: (dx, dy) => {
+         x += dx
+         y += dy
+      },
+      draw: () => {
+         const c = document.getElementById("graphpanel")
+         const ctx = c.getContext("2d")
+         ctx.beginPath()
+         ctx.moveTo(x+10, y)
+         ctx.lineTo(x+20, y+10)
+         ctx.lineTo(x+10, y+20)
+         ctx.lineTo(x, y+10)
+         ctx.fillStyle = 'red'
+         ctx.fill()
       }
-    },
-    contains: p => {
-      return (x+20/2-p.x)**2+(y+20/2-p.y)**2 <= 20**2/4
-    },
-    translate: (dx, dy) => {
-      x += dx
-      y += dy
-    },
-    draw: () => {
-      const c = document.getElementById("graphpanel")
-      const ctx = c.getContext("2d")
-      ctx.beginPath()
-      ctx.moveTo(x+10, y)
-      ctx.lineTo(x+20, y+10)
-      ctx.lineTo(x+10, y+20)
-      ctx.lineTo(x, y+10)
-      ctx.fillStyle = 'red'
-      ctx.fill()
-    }
-  }
+   }
 }
