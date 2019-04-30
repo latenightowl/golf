84e5aa811c8d73271bd6d12e937d374a8c9dfb2d
Thomas Zakh <tommyzakh@gmail.com>
2019-04-30 12:02:15 -0700

    Added toolbar class to html buttons

--- graphEditor/js/GraphFrame.js
+++ graphEditor/js/GraphFrame.js
@@ -9,72 +9,72 @@ function drawGrabber(x, y) {
    const c = document.getElementById("graphpanel");
    const ctx = c.getContext("2d");
    ctx.beginPath()
-   ctx.rect(x - size/2, y - size/2, size, size)
+   ctx.rect(x - size / 2, y - size / 2, size, size)
    ctx.fillStyle = 'black'
    ctx.fill()
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  const graph = new Graph()
-  const canvas = document.getElementById('graphpanel')
-  let dragStartPoint = undefined
-  let dragStartBounds = undefined
-  let selected = undefined
+   const graph = new Graph()
+   const canvas = document.getElementById('graphpanel')
+   let dragStartPoint = undefined
+   let dragStartBounds = undefined
+   let selected = undefined
 
-  function repaint() {
-    canvas.innerHTML = ''
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    graph.draw()
+   function repaint() {
+      canvas.innerHTML = ''
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      graph.draw()
 
-    if (selected !== undefined) {
-      const bounds = selected.getBounds()
-      drawGrabber(bounds.x, bounds.y)
-      drawGrabber(bounds.x + bounds.width, bounds.y)
-      drawGrabber(bounds.x, bounds.y + bounds.height)      
-      drawGrabber(bounds.x + bounds.width, bounds.y + bounds.height)
-    }
-  }
-   
-  function mouseLocation(event) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top,
-    }
-  }
+      if (selected !== undefined) {
+         const bounds = selected.getBounds()
+         drawGrabber(bounds.x, bounds.y)
+         drawGrabber(bounds.x + bounds.width, bounds.y)
+         drawGrabber(bounds.x, bounds.y + bounds.height)
+         drawGrabber(bounds.x + bounds.width, bounds.y + bounds.height)
+      }
+   }
 
-  canvas.addEventListener('mousedown', event => {
-    let mousePoint = mouseLocation(event)
-    dragStartPoint = mousePoint 
-    selected = graph.findNode(mousePoint)
-    if(option === 'select' && selected) { dragStartBounds = selected.getBounds() }
-    if(option === 'circle') { graph.add(createCircleNode(mousePoint.x, mousePoint.y)) }
-    if(option === 'diamond') { graph.add(createDiamondNode(mousePoint.x, mousePoint.y)) }
-    repaint()
-  })
-   
-  canvas.addEventListener('mouseup', event => {
-    let mousePoint = mouseLocation(event)
-    if(option === 'edge' && dragStartPoint != undefined) {
-      const e = createLineEdge()
-      graph.connect(e, dragStartPoint, mousePoint)
-    }
-    dragStartPoint = undefined
-    dragStartBounds = null
-    repaint()
-  })  
+   function mouseLocation(event) {
+      var rect = canvas.getBoundingClientRect();
+      return {
+         x: event.clientX - rect.left,
+         y: event.clientY - rect.top,
+      }
+   }
 
-  canvas.addEventListener('mousemove', event => {
-    let mousePoint = mouseLocation(event)
-    if (dragStartBounds != null) {
-      const bounds = selected.getBounds()
-      selected.translate(
-        dragStartBounds.x - bounds.x
-        + mousePoint.x - dragStartPoint.x,
-        dragStartBounds.y - bounds.y
-        + mousePoint.y - dragStartPoint.y)
-    }
-    repaint()
-  })
+   canvas.addEventListener('mousedown', event => {
+      let mousePoint = mouseLocation(event)
+      dragStartPoint = mousePoint
+      selected = graph.findNode(mousePoint)
+      if (option === 'select' && selected) { dragStartBounds = selected.getBounds() }
+      if (option === 'circle') { graph.add(createCircleNode(mousePoint.x, mousePoint.y)) }
+      if (option === 'diamond') { graph.add(createDiamondNode(mousePoint.x, mousePoint.y)) }
+      repaint()
+   })
+
+   canvas.addEventListener('mouseup', event => {
+      let mousePoint = mouseLocation(event)
+      if (option === 'edge' && dragStartPoint != undefined) {
+         const e = createLineEdge()
+         graph.connect(e, dragStartPoint, mousePoint)
+      }
+      dragStartPoint = undefined
+      dragStartBounds = null
+      repaint()
+   })
+
+   canvas.addEventListener('mousemove', event => {
+      let mousePoint = mouseLocation(event)
+      if (dragStartBounds != null) {
+         const bounds = selected.getBounds()
+         selected.translate(
+            dragStartBounds.x - bounds.x
+            + mousePoint.x - dragStartPoint.x,
+            dragStartBounds.y - bounds.y
+            + mousePoint.y - dragStartPoint.y)
+      }
+      repaint()
+   })
 })
