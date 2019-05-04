24a9b073af2a9eb14077d0d606b9ddb3fed501cb
Eugenie Zhu <eugeniezhu@gmail.com>
2019-05-03 20:45:15 -0700

    -added intial property sheet file

---
+++ umlEditor/js/PropertySheet.js
@@ -0,0 +1,28 @@
+function createProperty() {
+   const input = document.createElement("input")
+   return {
+      generateHTML() {
+         input.id = "color"
+         input.type = "text"
+         input.value = ""
+
+          const text = document.createElement("p")
+          text.innerHTML = "Color: "
+
+         let container = document.getElementById("property-container")
+         container.appendChild(text)
+         container.appendChild(input)
+      },
+      getColor(color) {
+         input.value = color
+      },
+      emptyField() {
+         input.value = ""
+      },
+      isColor(c) {
+         let s = new Option().style
+         s.color = c
+         return s.color === c
+      }
+   }
+}
