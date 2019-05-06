8bf543834f2a8b70f79dd09ce58c61ae890e80da
Thomas Zakh <tommyzakh@gmail.com>
2019-05-06 12:26:56 -0700

    Fixed note node bounds

--- umlEditor/js/GraphFrame.js
+++ umlEditor/js/GraphFrame.js
@@ -1,218 +1,240 @@
 // Enum generation code from TypeScript transpilation
-var TOOL_TYPE;
+var TOOL_TYPE
 
-(function(TOOL_TYPE) {
-  TOOL_TYPE[(TOOL_TYPE["select"] = 0)] = "select"
-  TOOL_TYPE[(TOOL_TYPE["edgeLine"] = 3)] = "edgeLine"
-  TOOL_TYPE[(TOOL_TYPE["edgeLineDot"] = 4)] = "edgeLineDot"
-  TOOL_TYPE[(TOOL_TYPE["edgeH"] = 5)] = "edgeH"
-  TOOL_TYPE[(TOOL_TYPE["edgeHDot"] = 6)] = "edgeHDot"
-  TOOL_TYPE[(TOOL_TYPE["edgeV"] = 7)] = "edgeV"
-  TOOL_TYPE[(TOOL_TYPE["edgeVDot"] = 8)] = "edgeVDot"
-  TOOL_TYPE[(TOOL_TYPE["nodeClass"] = 9)] = "nodeClass"
-  TOOL_TYPE[(TOOL_TYPE["nodeNote"] = 10)] = "nodeNote"
-  TOOL_TYPE[(TOOL_TYPE["nodeInterface"] = 11)] = "nodeInterface"
+;(function(TOOL_TYPE) {
+   TOOL_TYPE[(TOOL_TYPE["select"] = 0)] = "select"
+   TOOL_TYPE[(TOOL_TYPE["edgeLine"] = 3)] = "edgeLine"
+   TOOL_TYPE[(TOOL_TYPE["edgeLineDot"] = 4)] = "edgeLineDot"
+   TOOL_TYPE[(TOOL_TYPE["edgeH"] = 5)] = "edgeH"
+   TOOL_TYPE[(TOOL_TYPE["edgeHDot"] = 6)] = "edgeHDot"
+   TOOL_TYPE[(TOOL_TYPE["edgeV"] = 7)] = "edgeV"
+   TOOL_TYPE[(TOOL_TYPE["edgeVDot"] = 8)] = "edgeVDot"
+   TOOL_TYPE[(TOOL_TYPE["nodeClass"] = 9)] = "nodeClass"
+   TOOL_TYPE[(TOOL_TYPE["nodeNote"] = 10)] = "nodeNote"
+   TOOL_TYPE[(TOOL_TYPE["nodeInterface"] = 11)] = "nodeInterface"
 })(TOOL_TYPE || (TOOL_TYPE = {}))
 
 let currentTool = TOOL_TYPE.select
 
 function usingEdge() {
-  if (typeof currentTool === "string") {
-    return (
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeLine] ||
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeLineDot] ||
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeH] ||
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeV] ||
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeHDot] ||
-      currentTool === TOOL_TYPE[TOOL_TYPE.edgeVDot]
-    )
-  } else {
-    return (
-      currentTool === TOOL_TYPE.edgeLine ||
-      currentTool === TOOL_TYPE.edgeLineDot ||
-      currentTool === TOOL_TYPE.edgeH ||
-      currentTool === TOOL_TYPE.edgeV ||
-      currentTool === TOOL_TYPE.edgeHDot ||
-      currentTool === TOOL_TYPE.edgeVDot
-    )
-  }
+   if (typeof currentTool === "string") {
+      return (
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeLine] ||
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeLineDot] ||
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeH] ||
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeV] ||
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeHDot] ||
+         currentTool === TOOL_TYPE[TOOL_TYPE.edgeVDot]
+      )
+   } else {
+      return (
+         currentTool === TOOL_TYPE.edgeLine ||
+         currentTool === TOOL_TYPE.edgeLineDot ||
+         currentTool === TOOL_TYPE.edgeH ||
+         currentTool === TOOL_TYPE.edgeV ||
+         currentTool === TOOL_TYPE.edgeHDot ||
+         currentTool === TOOL_TYPE.edgeVDot
+      )
+   }
 }
 
 function setTool(newTool) {
-  currentTool = newTool
+   currentTool = newTool
 }
 
 function drawGrabber(ctx, bounds) {
-  drawGrabberCorner(ctx, bounds.x, bounds.y)
-  drawGrabberCorner(ctx, bounds.x + bounds.width, bounds.y)
-  drawGrabberCorner(ctx, bounds.x, bounds.y + bounds.height)
-  drawGrabberCorner(ctx, bounds.x + bounds.width, bounds.y + bounds.height)
+   drawGrabberCorner(ctx, bounds.x, bounds.y)
+   drawGrabberCorner(ctx, bounds.x + bounds.width, bounds.y)
+   drawGrabberCorner(ctx, bounds.x, bounds.y + bounds.height)
+   drawGrabberCorner(ctx, bounds.x + bounds.width, bounds.y + bounds.height)
 }
 
 function drawGrabberCorner(ctx, x, y) {
-  const size = 4
-  ctx.beginPath()
-  ctx.rect(x - size / 2, y - size / 2, size, size)
-  ctx.fillStyle = "black"
-  ctx.fill()
+   const size = 4
+   ctx.beginPath()
+   ctx.rect(x - size / 2, y - size / 2, size, size)
+   ctx.fillStyle = "black"
+   ctx.fill()
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  const toolbar = createToolbar(
-    createTBButton(TOOL_TYPE[TOOL_TYPE.select]),
-    createTBButton(TOOL_TYPE[TOOL_TYPE.edgeLine]),
-    createTBButton(TOOL_TYPE[TOOL_TYPE.edgeLineDot]),
-    createTBButton(TOOL_TYPE[TOOL_TYPE.nodeNote]),
-    createTBButton(TOOL_TYPE[TOOL_TYPE.nodeClass]),
-    createTBButton(TOOL_TYPE[TOOL_TYPE.nodeInterface]),
+   const toolbar = createToolbar(
+      createTBButton(TOOL_TYPE[TOOL_TYPE.select]),
+      createTBButton(TOOL_TYPE[TOOL_TYPE.edgeLine]),
+      createTBButton(TOOL_TYPE[TOOL_TYPE.edgeLineDot]),
+      createTBButton(TOOL_TYPE[TOOL_TYPE.nodeNote]),
+      createTBButton(TOOL_TYPE[TOOL_TYPE.nodeClass]),
+      createTBButton(TOOL_TYPE[TOOL_TYPE.nodeInterface])
+   )
+   toolbar.generateHTML()
 
-  )
-  toolbar.generateHTML()
+   const propBar = createClassProperty()
+   propBar.generateHTML()
 
-  const propBar = createClassProperty()
-  propBar.generateHTML()
+   const notePropBar = createNoteProperty()
+   notePropBar.generateHTML()
 
-  const notePropBar = createNoteProperty()
-  notePropBar.generateHTML()
+   const interfacePropBar = createInterfaceProperty()
+   interfacePropBar.generateHTML()
 
-  const interfacePropBar = createInterfaceProperty()
-  interfacePropBar.generateHTML()
+   const graph = new createGraph()
+   const canvas = document.getElementById("graphpanel")
+   let dragStartPoint = undefined
+   let dragStartBounds = undefined
+   let selected = undefined
 
-  const graph = new createGraph()
-  const canvas = document.getElementById("graphpanel")
-  let dragStartPoint = undefined
-  let dragStartBounds = undefined
-  let selected = undefined
+   const canvasStyle = getComputedStyle(canvas)
+   canvas.width = parseInt(canvasStyle.width)
+   canvas.height = parseInt(canvasStyle.height)
+   const ctx = canvas.getContext("2d")
 
-  const canvasStyle = getComputedStyle(canvas)
-  canvas.width = parseInt(canvasStyle.width)
-  canvas.height = parseInt(canvasStyle.height)
-  const ctx = canvas.getContext("2d")
-
-  window.addEventListener("resize", function() {
-    const canvasStyle = getComputedStyle(canvas)
-    canvas.width = parseInt(canvasStyle.width)
-    canvas.height = parseInt(canvasStyle.height)
-    repaint()
-  })
-
-  function repaint() {
-    let c = document.getElementById('nodeContainer');
-    if(c !== null){
-      c.innerHTML = '';
-    }
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    graph.draw()
-
-    if (selected) {
-      const bounds = selected.getBounds()
-      const ctx = canvas.getContext("2d")
-      drawGrabber(ctx, bounds)
-    }
-  }
-
-  function mouseLocation(event) {
-    var rect = canvas.getBoundingClientRect()
-    return {
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top
-    }
-  }
-
-  canvas.addEventListener("mousedown", event => {
-    let mousePoint = mouseLocation(event)
-    dragStartPoint = mousePoint
-    selected = graph.findNode(mousePoint)
-
-    if (currentTool === "select" && selected) {
-      dragStartBounds = selected.getBounds()
-    }
-
-    if (currentTool === "nodeClass") {
-      let newClass = createNodeClass()
-      newClass.translate(mousePoint.x, mousePoint.y)
-      graph.add(newClass)
-    }
-
-    if (currentTool === "nodeNote") {
-      let newNote = createNodeNote()
-      newNote.translate(mousePoint.x, mousePoint.y)
-      graph.add(newNote)
-    }
-
-    if (currentTool === "nodeInterface") {
-      let newInterface = createNodeInterface()
-      newInterface.translate(mousePoint.x, mousePoint.y)
-      graph.add(newInterface)
-    }
-
-    if (selected !== undefined) {
-      if (selected.getType() === "class") {
-        let input1 = document.getElementById("name")
-        input1.addEventListener("input", event => {selected.setText1(input1.value)})
-        let input2 = document.getElementById("attribute")
-        input2.addEventListener("input", event => {selected.setText2(input2.value)})
-        let input3 = document.getElementById("methods")
-        input3.addEventListener("input", event => {selected.setText3(input3.value)})
-
-        selected !== undefined ?
-          propBar.getText(selected.getText1(),selected.getText2(),selected.getText3()) : propBar.emptyField()
-      }
-
-      if (selected.getType() === "note") {
-        let input = document.getElementById("text")
-        input.addEventListener("input", event => {selected.setText(input.value)})
-
-        selected !== undefined ?
-          notePropBar.getText(selected.getText()) : notePropBar.emptyField()
-      }
-
-      if (selected.getType() === "interface") {
-        let input = document.getElementById("interfaceName")
-        input.addEventListener("input", event => {selected.setInterfaceText(input.value)})
-        let input1 = document.getElementById("interfaceMethods")
-        input1.addEventListener("input", event => {selected.setInterfaceMethodsText(input1.value)})
-
-        selected !== undefined ?
-          interfacePropBar.getText(selected.getText1(), selected.getText2()) : interfacePropBar.emptyField()
-      }
-    }
-  repaint()
-  })
-
-  canvas.addEventListener("mouseup", event => {
-    let mousePoint = mouseLocation(event)
-    if (usingEdge() && dragStartPoint != undefined) {
-      let typeName = currentTool
-      typeName = typeName.charAt(0).toUpperCase() + typeName.slice(1)
-      const e = window["create" + typeName]()
-      graph.connect(e, dragStartPoint, mousePoint)
+   window.addEventListener("resize", function() {
+      const canvasStyle = getComputedStyle(canvas)
+      canvas.width = parseInt(canvasStyle.width)
+      canvas.height = parseInt(canvasStyle.height)
       repaint()
-    }
+   })
 
-    dragStartPoint = undefined
-    dragStartBounds = null
-})
+   function repaint() {
+      let c = document.getElementById("nodeContainer")
+      if (c !== null) {
+         c.innerHTML = ""
+      }
 
-  canvas.addEventListener("mousemove", event => {
-    let mousePoint = mouseLocation(event)
-    if (dragStartBounds != null) {
-    const bounds = selected.getBounds()
-    selected.translate(
-      dragStartBounds.x - bounds.x + mousePoint.x - dragStartPoint.x,
-      dragStartBounds.y - bounds.y + mousePoint.y - dragStartPoint.y
-    )}
-    repaint()
-  })
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      graph.draw()
 
-  window.addEventListener("keydown", (event) => {
-    if (event.code === "Delete" || event.code === "Backspace") {
-    if (selected) {
-      graph.remove(selected)
-      selected = null
+      if (selected) {
+         const bounds = selected.getBounds()
+         const ctx = canvas.getContext("2d")
+         drawGrabber(ctx, bounds)
+      }
+   }
+
+   function mouseLocation(event) {
+      var rect = canvas.getBoundingClientRect()
+      return {
+         x: event.clientX - rect.left,
+         y: event.clientY - rect.top
+      }
+   }
+
+   canvas.addEventListener("mousedown", (event) => {
+      let mousePoint = mouseLocation(event)
+      dragStartPoint = mousePoint
+      selected = graph.findNode(mousePoint)
+
+      if (currentTool === "select" && selected) {
+         dragStartBounds = selected.getBounds()
+      }
+
+      if (currentTool === "nodeClass") {
+         let newClass = createNodeClass()
+         newClass.translate(mousePoint.x, mousePoint.y)
+         graph.add(newClass)
+      }
+
+      if (currentTool === "nodeNote") {
+         let newNote = createNodeNote()
+         newNote.translate(mousePoint.x, mousePoint.y)
+         graph.add(newNote)
+      }
+
+      if (currentTool === "nodeInterface") {
+         let newInterface = createNodeInterface()
+         newInterface.translate(mousePoint.x, mousePoint.y)
+         graph.add(newInterface)
+      }
+
+      if (selected !== undefined) {
+         if (selected.getType() === "class") {
+            let input1 = document.getElementById("name")
+            input1.addEventListener("input", (event) => {
+               selected.setText1(input1.value)
+            })
+            let input2 = document.getElementById("attribute")
+            input2.addEventListener("input", (event) => {
+               selected.setText2(input2.value)
+            })
+            let input3 = document.getElementById("methods")
+            input3.addEventListener("input", (event) => {
+               selected.setText3(input3.value)
+            })
+
+            selected !== undefined
+               ? propBar.getText(
+                    selected.getText1(),
+                    selected.getText2(),
+                    selected.getText3()
+                 )
+               : propBar.emptyField()
+         }
+
+         if (selected.getType() === "note") {
+            let input = document.getElementById("text")
+            input.addEventListener("input", (event) => {
+               selected.setText(input.value)
+            })
+
+            selected !== undefined
+               ? notePropBar.getText(selected.getText())
+               : notePropBar.emptyField()
+         }
+
+         if (selected.getType() === "interface") {
+            let input = document.getElementById("interfaceName")
+            input.addEventListener("input", (event) => {
+               selected.setInterfaceText(input.value)
+            })
+            let input1 = document.getElementById("interfaceMethods")
+            input1.addEventListener("input", (event) => {
+               selected.setInterfaceMethodsText(input1.value)
+            })
+
+            selected !== undefined
+               ? interfacePropBar.getText(
+                    selected.getText1(),
+                    selected.getText2()
+                 )
+               : interfacePropBar.emptyField()
+         }
+      }
       repaint()
-    }
-  }
-})
+   })
+
+   canvas.addEventListener("mouseup", (event) => {
+      let mousePoint = mouseLocation(event)
+      if (usingEdge() && dragStartPoint != undefined) {
+         let typeName = currentTool
+         typeName = typeName.charAt(0).toUpperCase() + typeName.slice(1)
+         const e = window["create" + typeName]()
+         graph.connect(e, dragStartPoint, mousePoint)
+         repaint()
+      }
+
+      dragStartPoint = undefined
+      dragStartBounds = null
+   })
+
+   canvas.addEventListener("mousemove", (event) => {
+      let mousePoint = mouseLocation(event)
+      if (dragStartBounds != null) {
+         const bounds = selected.getBounds()
+         selected.translate(
+            dragStartBounds.x - bounds.x + mousePoint.x - dragStartPoint.x,
+            dragStartBounds.y - bounds.y + mousePoint.y - dragStartPoint.y
+         )
+      }
+      repaint()
+   })
+
+   window.addEventListener("keydown", (event) => {
+      if (event.code === "Delete" || event.code === "Backspace") {
+         if (selected) {
+            graph.remove(selected)
+            selected = null
+            repaint()
+         }
+      }
+   })
 })
--- umlEditor/js/NoteNode.js
+++ umlEditor/js/NoteNode.js
@@ -1,88 +1,91 @@
 function createNodeNote() {
-  let width = 40
-  let height = 30
-  let type = "note"
-  let x = 0
-  let y = 0
-  let container = document.getElementById('button3')
-  let actualContainer = document.getElementById('nodeContainer')
-  let table = document.createElement('table')
-  let tr = document.createElement('tr')
-  let td = document.createElement('th')
-  td.innerText = "Note"
-  table.bgColor = "yellow"
+   let width = 40
+   let height = 30
+   let type = "note"
+   let x = 0
+   let y = 0
+   let container = document.getElementById("button3")
+   let actualContainer = document.getElementById("nodeContainer")
+   let table = document.createElement("table")
+   let tr = document.createElement("tr")
+   let td = document.createElement("th")
+   td.innerText = "Note"
+   table.bgColor = "yellow"
 
-  return {
-    getType: () => {
-    return type
-  },
+   return {
+      getType: () => {
+         return type
+      },
 
-    getBounds: () => {
-    return {
-      x: x,
-      y: y,
-      width: width,
-      height: height,
-      center: () => {
-      return { x: x + width / 2, y: y + height / 2 }
-    }
-  }
-  },
+      getBounds: () => {
+         return {
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            center: () => {
+               return { x: x + width / 2, y: y + height / 2 }
+            }
+         }
+      },
 
-  contains: p => {
-    if(p.x >= x && p.x <= (x+width) && p.y >= y && p.y <= y+height)
-      return true
-    else
-      return false
-  },
+      contains: (p) => {
+         if (p.x >= x && p.x <= x + width && p.y >= y && p.y <= y + height)
+            return true
+         else return false
+      },
 
-  translate: (dx, dy) => {
-    x += dx
-    y += dy
-  },
+      translate: (dx, dy) => {
+         x += dx
+         y += dy
+      },
 
-  draw: () => {
-    table.appendChild(tr)
-    tr.appendChild(td)
-    table.style.position = 'absolute'
-    table.style.left = x + 'px'
-    table.style.top = y + 'px'
-    table.style.width = width + 'px'
-    table.style.height = height + 'px'
-    actualContainer.appendChild(table)
-  },
+      draw: () => {
+         table.appendChild(tr)
+         tr.appendChild(td)
+         table.style.position = "absolute"
+         table.style.left = x + "px"
+         table.style.top = y + "px"
+         table.style.width = width + "px"
+         table.style.height = height + "px"
+         actualContainer.appendChild(table)
 
-  getConnectionPoint: other => {
-    let centerX = x + width / 2
-    let centerY = y + height / 2
-    let dx = other.getBounds().center().x - x
-    let dy = other.getBounds().center().y - y
-    if (dx >= dy && dx >= -dy) return { x: x + width, y: centerY }
-    if (dx < dy && dx >= -dy) return { x: centerX, y: y + height }
-    if (dx >= dy && dx < -dy) return { x: centerX, y: y }
-    return { x, y: centerY }
-  },
+         const tableStyle = getComputedStyle(table)
+         width = parseInt(tableStyle.width)
+         height = parseInt(tableStyle.height)
+      },
 
-  getText: noteText => {
-    return td.innerText
-  },
+      getConnectionPoint: (other) => {
+         let centerX = x + width / 2
+         let centerY = y + height / 2
+         let dx = other.getBounds().center().x - x
+         let dy = other.getBounds().center().y - y
+         if (dx >= dy && dx >= -dy) return { x: x + width, y: centerY }
+         if (dx < dy && dx >= -dy) return { x: centerX, y: y + height }
+         if (dx >= dy && dx < -dy) return { x: centerX, y: y }
+         return { x, y: centerY }
+      },
 
-  setText: noteText => {
-    td.innerText = noteText
-  },
-  scale: (factor) => {
-    width *= factor
-    height *= factor
-  },
-  toolBarDraw: () => {
-    table.appendChild(tr)
-    tr.appendChild(td)
-    table.style.position = 'absolute'
-    table.style.left = x + 'px'
-    table.style.top = y + 'px'
-    table.style.width = width + 'px'
-    table.style.height = height + 'px'
-    container.appendChild(table)
-  }
-}
+      getText: (noteText) => {
+         return td.innerText
+      },
+
+      setText: (noteText) => {
+         td.innerText = noteText
+      },
+      scale: (factor) => {
+         width *= factor
+         height *= factor
+      },
+      toolBarDraw: () => {
+         table.appendChild(tr)
+         tr.appendChild(td)
+         table.style.position = "absolute"
+         table.style.left = x + "px"
+         table.style.top = y + "px"
+         table.style.width = width + "px"
+         table.style.height = height + "px"
+         container.appendChild(table)
+      }
+   }
 }
